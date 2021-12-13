67a3b8406d6eb4e686111794cd26c2cd440dc682
Rita Ogedegbe <ritaogedegbe@yahoo.co.uk>
2021-12-12 18:39:47 -0700

    Adds styling to the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const [counter, setCounter] = useState(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const onChangeIncrement = () => {
+    setCounter((counter) => counter + 1);
+  };
+
+  const onChangeDecrement = () => {
+    setCounter((counter) => {
+      if (counter <= 0) return 0;
+      return counter - 1;
+    });
+  };
+
+  return (
+    <div className="body">
+      <div className="container">
+        <button className="increment" onClick={onChangeIncrement}>
+          Increment
+        </button>
+        <span className="counter">{counter}</span>
+        <button className="decrement" onClick={onChangeDecrement}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.querySelector("#root"));
